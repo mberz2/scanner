3c1b27125cdff4cbeb63e0edf995a391f347a89c
pasomateo <mberz2@pdx.edu>
2021-11-08 15:47:15 -0800

    Added location tracking.

--- app/src/main/java/com/scanner/MainActivity.java
+++ app/src/main/java/com/scanner/MainActivity.java
@@ -1,20 +1,47 @@
 package com.scanner;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     //Initialize variable
     Button btScan;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    String location_string;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +51,23 @@ public class MainActivity extends AppCompatActivity {
         //Assign variable
         btScan = findViewById(R.id.bt_scan);
 
+        //Initialize fusedLocationProviderClient
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
         btScan.setOnClickListener(view -> {
+
+            //Check location permissions
+            if(ActivityCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+                getLocation();
+            } else {
+                //Permission denied
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+            }
+
+
             //Initialize intent integrator
             IntentIntegrator intentIntegrator = new IntentIntegrator(
                     MainActivity.this
@@ -47,6 +90,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(task -> {
+            //Initialize location
+            Location location = task.getResult();
+            if (location != null) {
+                //Initialize geoCoder
+                Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                //Initialize address list
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(
+                            location.getLatitude(), location.getLongitude(), 1);
+                    location_string = addresses.get(0).getLatitude() +
+                            " " + addresses.get(0).getLongitude() +
+                            "\nCountry: " + addresses.get(0).getCountryName() +
+                            "\nLocality: " + addresses.get(0).getLocality();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        }
+
+        @SuppressLint("MissingPermission")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -66,16 +133,16 @@ public class MainActivity extends AppCompatActivity {
             //Set Activity type
             String activityType = "Observe";
 
-
-
             //Set title
             builder.setTitle("Scan Result");
 
             //Set message
-            //builder.setMessage(intentResult.getContents());
-            builder.setMessage("#: "+intentResult.getContents()
+            builder.setMessage(
+                    "#: "+intentResult.getContents()
                     +"\nType: "+intentResult.getFormatName()
-                    +"\nActivity: "+activityType);
+                    +"\nActivity: "+ activityType
+                    +"\n"+location_string
+            );
 
 
             //Set positive button
@@ -97,6 +164,4 @@ public class MainActivity extends AppCompatActivity {
                     .show();
         }
     }
-
-
 }
